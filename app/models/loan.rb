bd32263bed9f4f11a09f5d66d50e91f2613b3ff1
SumLare <anohin56n@gmail.com>
2018-02-01 02:48:55 +0300

    Notify with sms

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -11,6 +11,13 @@ class Loan < ApplicationRecord
   validates :amount, :term, numericality: { greater_than: 0 }
   validates :term, numericality: { less_than_or_equal_to: 12 }
 
+  scope :with_enabled_sms_notifications, -> { where(sms_notifications_enabled: true) }
+  scope :with_enabled_email_notifications, -> { where(email_notifications_enabled: true) }
+
+  def self.notifiable
+    where('? between date(next_date_of_payment) - 5 and next_date_of_payment', Time.zone.now)
+  end
+
   def full_name
     "#{last_name} #{first_name} #{patronymic_name}"
   end
@@ -27,8 +34,18 @@ class Loan < ApplicationRecord
     amount * MONTHLY_RATE / (1 - 1 / (1 + MONTHLY_RATE) ** term)
   end
 
-  def next_payment_date
+  def calculate_next_date_of_payment
     created_at + payments.paid.count.months
+  end
+
+  def notify_with_sms
+    SMSNotifier.new(phone_number, sms_content).notify
+    update_column(:notified, true)
+  end
+
+  def notify_with_email
+    LoanMailer.date_of_payment_reminder(self).deliver_later
+    update_column(:notified, true)
   end
 
   private
@@ -38,5 +55,9 @@ class Loan < ApplicationRecord
     user = User.find_or_create_by!(email: email) { |user| user.password = generated_password }
     self.user = user
     UserMailer.new_user(user, generated_password).deliver_later
+  end
+
+  def sms_content
+    "Напомнинание о дате платежа для займа №#{id}. Дата платежа #{next_payment_date}"
   end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,5 +3,13 @@ class Payment < ApplicationRecord
 
   validates :amount, numericality: { greater_than: 0 }
 
+  after_save :set_next_date_of_payment
+
   scope :paid, -> { where(paid: true) }
+
+  private
+
+  def set_next_date_of_payment
+    loan.update_column(:next_date_of_payment, loan.calculate_next_date_of_payment)
+  end
 end
